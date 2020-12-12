8736655f04d27eea520803c65b389fbb1ba53e20
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-12-12 22:16:41 +0100

    added logout button to navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -21,10 +21,24 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
             Support</a>
-        <a class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex" href="{{ route('login') }}">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 mr-1">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
-            </svg>
-            Login/Register</a>
+        @guest
+            <a class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex" href="{{ route('login') }}">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 mr-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
+                </svg>
+                Login/Register
+            </a>  
+        @endguest
+        @auth
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <a class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex" href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 mr-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                  </svg>
+                Logout
+            </a> 
+        </form>
+        @endauth
     </div>
 </nav>
