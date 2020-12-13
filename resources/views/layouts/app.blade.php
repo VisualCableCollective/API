6330a9c49b17f64947a4c005f83abc8b3d5207e6
joschiservice <joschua.hass.sh@gmail.com>
2020-12-13 11:49:51 +0100

    changed successful auth redirect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,13 +4,15 @@
     @include("components.navbar")
 
     <!-- Page Heading -->
+    {{-- 
     @auth
     <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             {{ $header }}
         </div>
     </header>
-    @endauth
+    @endauth 
+    --}}
 
     <!-- Page Content -->
     <main>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
