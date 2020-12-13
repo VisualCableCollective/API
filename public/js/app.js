791929a2d5352b79181c4bf0886668830c89db8f
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-12-13 17:42:52 +0100

    added my account modal and modal functions

--- public/js/app.js
+++ public/js/app.js
@@ -33830,6 +33830,48 @@ $(".onclick-spinner").on("click", function () {
   $("#" + caller_id + "BtnText").html($(this).data('loading-text'));
   $(this).closest("form").trigger('submit');
 });
+$('.modal-toggler').on("click", function () {
+  var target = $(this).data('target');
+
+  if ($(target + "-wrapper").hasClass("hidden")) {
+    OpenModal(target);
+  } else {
+    CloseModal(target);
+  }
+});
+$('.modal-bg').on("click", function () {
+  CloseModal($(this).data('modal'));
+});
+
+function OpenModal(target) {
+  $(target + "-wrapper").removeClass("hidden"); //bg
+
+  $(target + "-bg").removeClass("ease-in duration-200");
+  $(target + "-bg").addClass("ease-out duration-300");
+  $(target + "-bg").removeClass("opacity-0");
+  $(target + "-bg").addClass("opacity-100"); //modal
+
+  $(target).removeClass("ease-in duration-200");
+  $(target).addClass("ease-out duration-300");
+  $(target).removeClass("opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95");
+  $(target).addClass("opacity-100 translate-y-0 sm:scale-100");
+}
+
+function CloseModal(target) {
+  //bg
+  $(target + "-bg").removeClass("ease-out duration-300");
+  $(target + "-bg").addClass("ease-in duration-200");
+  $(target + "-bg").removeClass("opacity-100");
+  $(target + "-bg").addClass("opacity-0"); //modal
+
+  $(target).removeClass("ease-out duration-300");
+  $(target).addClass("ease-in duration-200");
+  $(target).removeClass("opacity-100 translate-y-0 sm:scale-100");
+  $(target).addClass("opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95");
+  setTimeout(function () {
+    $(target + "-wrapper").addClass("hidden");
+  }, 200);
+}
 
 /***/ }),
 
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,3 +1,64 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<div class="fixed z-10 inset-0 overflow-y-auto hidden modal" id="my-account-modal-wrapper">
+    <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+      <!--
+        Background overlay, show/hide based on modal state.
+  
+        Entering: "ease-out duration-300"
+          From: "opacity-0"
+          To: "opacity-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100"
+          To: "opacity-0"
+      -->
+      <div class="fixed inset-0 transition-opacity ease-out duration-300 modal-bg opacity-0" data-modal="#my-account-modal" id="my-account-modal-bg" aria-hidden="true">
+        <div class="absolute inset-0 bg-black opacity-75"></div>
+      </div>
+  
+      <!-- This element is to trick the browser into centering the modal contents. -->
+      <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+      <!--
+        Modal panel, show/hide based on modal state.
+  
+        Entering: "ease-out duration-300"
+          From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          To: "opacity-100 translate-y-0 sm:scale-100"
+        Leaving: "ease-in duration-200"
+          From: "opacity-100 translate-y-0 sm:scale-100"
+          To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+      -->
+      <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95 ease-out duration-300" role="dialog" aria-modal="true" aria-labelledby="modal-headline" id="my-account-modal">
+        <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+          <div class="sm:flex sm:items-start">
+            <div class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+              <!-- Heroicon name: exclamation -->
+              <svg class="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+              </svg>
+            </div>
+            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+              <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-headline">
+                Deactivate account
+              </h3>
+              <div class="mt-2">
+                <p class="text-sm text-gray-500">
+                  Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+            Deactivate
+          </button>
+          <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 <nav class="navbar-main fixed w-full">
     <div class="flex justify-between mx-auto max-w-screen-md py-2 items-center">
         <a href="{{url('/')}}">
@@ -32,15 +93,13 @@
             </a>  
         @endguest
         @auth
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <a class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex" href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 mr-1">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                  </svg>
-                Logout
-            </a> 
-        </form>
+        <button role="button" class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex items-center modal-toggler" data-target="#my-account-modal">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="inline h-5 mr-1">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg>
+            My Account
+            <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+        </button>
         @endauth
     </div>
 </nav>
