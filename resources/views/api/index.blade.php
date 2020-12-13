a32ae82c0a6bc342fa06a0346cc1014a9cc5d2fd
joschiservice <joschua.hass.sh@gmail.com>
2020-12-13 13:29:07 +0100

    activated API feature

--- resources/views/api/index.blade.php
+++ resources/views/api/index.blade.php
@@ -1,13 +1,8 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('API Tokens') }}
-        </h2>
-    </x-slot>
-
-    <div>
-        <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
-            @livewire('api.api-token-manager')
-        </div>
+@extends('layouts.app')
+@section('page-content')
+<div>
+    <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+        @livewire('api.api-token-manager')
     </div>
-</x-app-layout>
+</div>
+@endsection
