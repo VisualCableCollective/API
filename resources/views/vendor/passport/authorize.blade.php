16ffdd099a6b7e170db0cfc984b79ac236395d9d
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2020-12-14 15:18:28 +0100

    finished oauth authorize page design

--- resources/views/vendor/passport/authorize.blade.php
+++ resources/views/vendor/passport/authorize.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.basic')
 @section('content')
     <div class="bg-dark-1 min-h-screen w-full flex items-center justify-center">
-        <div class="bg-dark-3 rounded-lg text-white p-12 shadow-md max-w-md">
+        <div class="bg-dark-3 rounded-lg text-white p-12 shadow-md max-w-sm">
             <div class="flex justify-center pb-4">
                 <img src="/img/branding/vcc/logo-transparent.png" alt="VCC Logo" class="h-14">
             </div>
-            <h1 class="text-xl pb-3"><strong>{{ $client->name }}</strong> is requesting permission to access your account.</h1>
-            <div class="flex w-full justify-between">
-                <form method="post" action="{{ route('passport.authorizations.approve') }}">
+            <h1 class="text-xl pb-3"><strong>{{ $client->name }}</strong> is requesting permission to access your VCC account.</h1>
+            <div class="flex w-full justify-between mt-3">
+                <form method="post" action="{{ route('passport.authorizations.approve') }}" class="flex-1 mr-2">
                     @csrf
     
                     <input type="hidden" name="state" value="{{ $request->state }}">
                     <input type="hidden" name="client_id" value="{{ $client->id }}">
                     <input type="hidden" name="auth_token" value="{{ $authToken }}">
-                    <button type="button" class="bg-blue-600 hover:bg-blue-700 w-full p-3 rounded transition-all duration-100 mt-3 onclick-spinner no-validation" id="AuthorizingSubmitBtn" data-loading-text="Authorizing...">
+                    <button type="button" class="bg-green-600 hover:bg-green-700 w-full p-3 rounded transition-all duration-100 onclick-spinner no-validation" id="AuthorizingSubmitBtn" data-loading-text="Authorizing...">
                         <div class="flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 38 38" stroke="#fff" class="pr-2 hidden" id="AuthorizingSubmitLoadingSVG">
                                 <g fill="none" fill-rule="evenodd">
@@ -29,14 +29,14 @@
                         </div>
                     </button>
                 </form>
-                <form method="post" action="{{ route('passport.authorizations.deny') }}">
+                <form method="post" action="{{ route('passport.authorizations.deny') }}" class="flex-1 ml-2">
                     @csrf
                     @method('DELETE')
     
                     <input type="hidden" name="state" value="{{ $request->state }}">
                     <input type="hidden" name="client_id" value="{{ $client->id }}">
                     <input type="hidden" name="auth_token" value="{{ $authToken }}">
-                    <button type="button" class="bg-red-600 hover:bg-red-700 w-full p-3 rounded transition-all duration-100 mt-3 onclick-spinner no-validation" id="CancelingSubmitBtn" data-loading-text="Canceling...">
+                    <button type="button" class="bg-red-600 hover:bg-red-700 w-full p-3 rounded transition-all duration-100 onclick-spinner no-validation" id="CancelingSubmitBtn" data-loading-text="Canceling...">
                         <div class="flex items-center justify-center">
                             <svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 38 38" stroke="#fff" class="pr-2 hidden" id="CancelingSubmitLoadingSVG">
                                 <g fill="none" fill-rule="evenodd">
