7a04e7af4d97683a2ba5ee9459ffbefecfc22c2b
joschiservice <joschua.hass.sh@gmail.com>
2020-12-14 08:33:36 +0100

    disabled Laravel Sanctum and replaced it with Laravel Passport

--- routes/api.php
+++ routes/api.php
@@ -17,24 +17,6 @@ use Illuminate\Validation\ValidationException;
 |
 */
 
-Route::post('/token', function (Request $request) {
-    $request->validate([
-        'email' => 'required|email',
-        'password' => 'required',
-        'device_name' => 'required',
-    ]);
-
-    $user = User::where('email', $request->email)->first();
-
-    if (! $user || ! Hash::check($request->password, $user->password)) {
-        throw ValidationException::withMessages([
-            'email' => ['The provided credentials are incorrect.'],
-        ]);
-    }
-
-    return $user->createToken($request->device_name)->plainTextToken;
-});
-
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
