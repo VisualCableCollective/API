2be7ed255734380951939587bad444a1fa9a0f71
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2021-01-01 12:43:58 +0100

    added link to products

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -78,7 +78,7 @@
         <a href="{{url('/')}}">
             <img src="/img/branding/vcc/logo-transparent.png" alt="VTCM logo" class="h-8">
         </a>
-        <a class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex" href="#">
+        <a class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex" href="/#products">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 mr-1">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
             </svg>
