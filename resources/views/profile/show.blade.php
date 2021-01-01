f6f856379330a3da4ff22fe53b07d7c557f874b6
Joschua H <54814804+TheNorthWestMediaGroup@users.noreply.github.com>
2021-01-01 13:10:11 +0100

    fixed user settings

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
-@section('page-content')
+@section('content')
+@parent
 <div class="pt-10">
     <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
         @if (Laravel\Fortify\Features::canUpdateProfileInformation())
