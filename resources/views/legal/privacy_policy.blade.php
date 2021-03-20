1ae4ee07bb1a4b68d40d96d9fe3225e2662aea74
joschiservice <joschua.hass.sh@gmail.com>
2021-03-20 20:01:38 +0100

    add opt out for matamo

--- resources/views/legal/privacy_policy.blade.php
+++ resources/views/legal/privacy_policy.blade.php
@@ -86,6 +86,10 @@
             <p>Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.</p>
 
             <p>The VisualCable Collective does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.</p>
+            <iframe
+            style="border: 0; height: 200px; width: 600px;"
+            src="https://metrics.vcc-online.eu/index.php?module=CoreAdminHome&action=optOut&language=en&backgroundColor=1c1c1c&fontColor=ffffff&fontSize=&fontFamily=Nunito"
+            ></iframe>    
         </div>
     </div>
 </div>
