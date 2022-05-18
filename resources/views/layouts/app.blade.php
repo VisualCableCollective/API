4d8cd70ee65117dce43e6db484f3b58fc7d9f614
Joschua H <joschua.hass.sh@gmail.com>
2022-05-18 09:44:21 +0200

    fix overscroll color

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.basic')
 @section('before-content')
-<div class="min-h-screen bg-gray-100">
+<div class="min-h-screen">
     @include("components.navbar")
 
     <!-- Page Heading -->
-    {{-- 
+    {{--
     @auth
     <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             {{ $header }}
         </div>
     </header>
-    @endauth 
+    @endauth
     --}}
 
     <!-- Page Content -->
