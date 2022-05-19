a9f1951df4a90dcbc5ce0a561c4f0012823fabbd
Joschua H <joschua.hass.sh@gmail.com>
2022-05-19 11:34:44 +0200

    added website link button to VTCM

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,14 +11,25 @@
     <h1 class="mx-auto text-center text-2xl sm:text-5xl py-3 font-bold">Discover Our Products</h1>
     <hr class="border-gray-800 pb-5 sm:pb-8">
     <div class="product">
-        <a href="https://vtcmanager.eu" class="sm:flex min-h-25vh">
-            <div class="sm:w-1/2 mx-5">
+        <div class="sm:flex min-h-25vh">
+            <div class="sm:flex-1 mx-5">
                 <h1 class="mx-auto text-center text-2xl sm:text-3xl font-bold">VTCManager</h1>
-                <p>VTCManager is a fast, reliable tracker for Euro Truck Simulator 2 and American Truck Simulator. The software makes it easier to manage virtual transport companies and adds other unique features to the gaming experience.</p>
+                <p>
+                    VTCManager is a fast, reliable tracker for Euro Truck Simulator 2 and American Truck Simulator.
+                    The software makes it easier to manage virtual transport companies and adds other unique features to
+                    the gaming experience.
+                </p>
+                <div class="mt-6">
+                    <a href="https://vtcmanager.eu" target="_blank" class="inline-block">
+                        <p class="bg-blue-700 hover:bg-blue-600 shadow-lg hover:shadow-blue-600/50 shadow-blue-700/50 rounded-lg font-bold py-2 px-4">
+                            <i class="fa-solid fa-up-right-from-square mr-2"></i> View Product
+                        </p>
+                    </a>
+                </div>
             </div>
-            <div class="sm:w-1/2 sm:h-auto h-72 bg-products-vtcm-home bg-cover bg-center mt-4 sm:mt-0">
-            </div>
-        </a>
+            <a href="https://vtcmanager.eu" class="sm:flex-1 sm:h-auto h-72 bg-products-vtcm-home bg-cover bg-center mt-4 sm:mt-0">
+            </a>
+        </div>
     </div>
     <!-- <hr class="border-gray-800 mt-5 pb-5"> -->
 </div>
