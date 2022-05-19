22040c597ec7e4da9766f4771d01d8891f4b7f52
Joschua H <joschua.hass.sh@gmail.com>
2022-05-19 11:46:44 +0200

    navbar: "about us" redirect to 404

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -88,7 +88,7 @@
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z" />
             </svg>
             News</a>
-        <a class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex" href="#">
+        <a class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex" href="/about">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-5 mr-1">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
             </svg>
@@ -104,7 +104,7 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1" />
                 </svg>
                 Login/Register
-            </a>  
+            </a>
         @endguest
         @auth
         <button role="button" class="text-gray-300 text-sm hover:text-white transition-colors ease-out duration-200 flex items-center modal-toggler" data-target="#my-account-modal">
