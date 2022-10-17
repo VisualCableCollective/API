d10f1c8fe926f9825b16bfbf0ba48a8f13abbebd
Joschua H <54814804+joschiservice@users.noreply.github.com>
2022-10-17 10:38:39 +0200

    tweak(dark-mode): dark inputs

--- resources/views/vendor/jetstream/components/input.blade.php
+++ resources/views/vendor/jetstream/components/input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'form-input rounded-md shadow-sm text-black']) !!}>
+<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'form-input shadow-inner focus:shadow rounded w-full py-2 px-3 placeholder-gray-400 bg-dark-4 focus:bg-dark-5 transition-all duration-75 outline-none text-white']) !!}>
